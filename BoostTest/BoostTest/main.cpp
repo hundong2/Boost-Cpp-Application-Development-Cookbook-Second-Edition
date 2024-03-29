fd135f59cb746a4c8de406841100f894743ea7db
hundong2 <ehdgns104@gmail.com>
2024-03-29 21:57:51 +0900

    Add boost information

---
+++ BoostTest/BoostTest/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  BoostTest
+//
+//  Created by songdong on 2024/02/18.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
